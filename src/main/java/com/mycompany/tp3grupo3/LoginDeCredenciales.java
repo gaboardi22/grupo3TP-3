48e410715a186e678d1e1be00dd462c53e3534f8
Ezequiel <ezequielaravena39@gmail.com>
2024-08-28 21:30:47 -0300

    renombrado para no confundirse con el ejercicio 2 del tp 3

--- src/main/java/com/mycompany/tp3grupo3/LoginDeCredenciales.java
+++ src/main/java/com/mycompany/tp3grupo3/LoginDeCredenciales.java
@@ -11,12 +11,12 @@ import javax.swing.JOptionPane;
  *
  * @author Ezequiel
  */
-public class Main extends javax.swing.JFrame {
-
+public class LoginDeCredenciales extends javax.swing.JFrame {
+    
     /**
      * Creates new form Main
      */
-    public Main() {
+    public LoginDeCredenciales() {
         initComponents();
     }
 
@@ -249,20 +249,21 @@ public class Main extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(Main.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(LoginDeCredenciales.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(Main.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(LoginDeCredenciales.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(Main.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(LoginDeCredenciales.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(Main.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(LoginDeCredenciales.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new Main().setVisible(true);
+                new LoginDeCredenciales().setVisible(true);
             }
         });
     }
